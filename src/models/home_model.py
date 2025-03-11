b35c667802f7b426c0ab045937fdb3478ce58d01
aem2231 <mariojetullaji@gmail.com>
2025-03-11 14:26:11 +0000

    help me

--- src/models/home_model.py
+++ src/models/home_model.py
@@ -9,7 +9,11 @@ class HomeModel:
         self.Helper = Helper()
 
     def poke_on_click_handler(self, button: str) -> None:
-        print(f"{button} clicked.")
+        message: dict[str, str] = {
+            "Title": "Add Pokemon",
+            "Message": "Use the search bar to search for and add a pokemon"
+        }
+        Helper.show_popup(message)
 
     def search(self, query: str) -> None:
         print(f"search button clicked: {query}")
